a0fcd1c33a374fb07bf8c7516f9b84ef3f6af564
leVert <selmamathis15@gmail.com>
2024-08-06 16:42:31 +0300

    get method with next server action on a form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,30 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+
 export default function SnippetCreatePage(){
+    async function createSnippet(formData:FormData) {
+        //server action directive
+        'use server'
+        //check the user inputs and make sure theyre valid
+        const title =formData.get('title') as string;
+        const code = formData.get('code') as string;
+
+        //create a new record in the database
+        const snippet = await db.snippet.create({
+            data:{
+                title,
+                code
+            }
+        });
+        console.log(snippet);
+
+        //Redirect user back to root route
+        redirect('/');
+        
+    }
     return(
-        <form>
+        <form action={createSnippet}>
             <h3 className="font-bold m-3">Create a Snippet</h3>
             <div className="flex flex-col gap">
                 <div className="flex gap-4">
