680ec90e823d46e926b5229f2f5104caa12f668e
leVert <selmamathis15@gmail.com>
2024-08-14 21:51:31 +0300

    link edit page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface SnippetShowPageProps{
@@ -17,6 +18,17 @@ export default async function SnippetShowPage(props: SnippetShowPageProps){
     }
 
     return(
-        <div>{snippet.title}</div>
+        <div>
+            <div className="flex m-4 justify-between items-center">
+                <h1 className="text-xl font-bold">{snippet.title}</h1>
+                <div className="flex pag-4">
+                    <Link href={`/snippets/${snippet.id}/edit`}className="p-2 border rounded">Edit</Link>
+                    <button className="p-2 border rounded">Delete</button>
+                </div>
+            </div>
+            <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+                <code>{snippet.code}</code>
+            </pre>
+        </div>
     );
 }
