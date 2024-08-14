7dedd726950ee148a7cace6b715ca0ab49c29f2f
leVert <selmamathis15@gmail.com>
2024-08-14 20:44:40 +0300

    fetch particular records, notFound function

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,7 +1,21 @@
-export default function SnippetShowPage(props: any){
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 
-    console.log(props);
+interface SnippetShowPageProps{
+    params:{
+        id: string
+    }
+}
+export default async function SnippetShowPage(props: SnippetShowPageProps){
+    const snippet = await db.snippet.findFirst({
+        where: {id: parseInt(props.params.id)}
+    })
+
+    if(!snippet){
+        return notFound();
+    }
+
     return(
-        <div>Show a snippet</div>
+        <div>{snippet.title}</div>
     );
 }
