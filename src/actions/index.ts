f9250f5f98500d1c1de94dea3d8c245c1efad8ae
leVert <selmamathis15@gmail.com>
2024-08-18 16:24:40 +0300

    error check with trycatch

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -24,25 +24,40 @@ export async function createSnippet(formState :{message:string}, formData:FormDa
     const title =formData.get('title')
     const code = formData.get('code')
  //validation with the useform
-    if(typeof title!=='string' || title.length<3){
-        return{
-            message: 'Title must be longer'
-        };
-    }
-
-    if(typeof code !=='string' || code.length<10){
-        return{
-            message: "Code Must be longer"
-        };
-    }
-    //create a new record in the database
-    const snippet = await db.snippet.create({
-        data:{
-            title,
-            code
+    try {
+        if(typeof title!=='string' || title.length<3){
+            return{
+                message: 'Title must be longer'
+            };
         }
-    });
-    console.log(snippet);
+    
+        if(typeof code !=='string' || code.length<10){
+            return{
+                message: "Code Must be longer"
+            };
+        }
+        // throw new Error ("Cant connect to db!"); //simulate error
+        //create a new record in the database
+        const snippet = await db.snippet.create({
+            data:{
+                title,
+                code
+            }
+        });
+        console.log(snippet);
+        
+    } catch (err: unknown) {
+        if(err instanceof Error){
+            return{
+                message: err.message,
+            };
+        }else{
+            return{
+                message: 'Something went wrong',
+            };
+        }
+        
+    }
 
     //Redirect user back to root route
     redirect('/');
