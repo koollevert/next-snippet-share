d9462f87ad78d5f2e015f013265ad658ff3ddb59
leVert <selmamathis15@gmail.com>
2024-08-18 13:27:48 +0300

    Update piece of state; useState

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -2,12 +2,18 @@
 
 import type { Snippet } from "@prisma/client";
 import Editor from '@monaco-editor/react';
+import { useState } from "react";
 
 interface SnippetEditFormProps{
     snippet: Snippet;
 }
 
 export default function SnippetEditForm({snippet}:SnippetEditFormProps){
+    const [code, setCode]=useState(snippet.code);
+
+    const handleEditorChange=(value: string="")=>{
+        setCode(value);
+    }
     return(
         <div>
             <Editor
@@ -15,6 +21,7 @@ export default function SnippetEditForm({snippet}:SnippetEditFormProps){
                 theme="vs-dark"
                 language='javascript'
                 defaultValue={snippet.code}
+                onChange={handleEditorChange}
             />
         </div>
         
