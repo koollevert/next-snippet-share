fa4df59299bbc601e76dbe47e78a0cfa837a409d
leVert <selmamathis15@gmail.com>
2024-08-18 15:00:01 +0300

    delete snippet

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { deleteSnippet } from "@/actions";
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -17,13 +18,17 @@ export default async function SnippetShowPage(props: SnippetShowPageProps){
         return notFound();
     }
 
+
+    const deleteSnippetAction=deleteSnippet.bind(null,snippet.id)
     return(
         <div>
             <div className="flex m-4 justify-between items-center">
                 <h1 className="text-xl font-bold">{snippet.title}</h1>
                 <div className="flex pag-4">
                     <Link href={`/snippets/${snippet.id}/edit`}className="p-2 border rounded">Edit</Link>
-                    <button className="p-2 border rounded">Delete</button>
+                    <form action={deleteSnippetAction}>
+                        <button className="p-2 border rounded">Delete</button>
+                    </form>
                 </div>
             </div>
             <pre className="p-3 border rounded bg-gray-200 border-gray-200">
